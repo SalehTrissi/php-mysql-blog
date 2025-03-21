d100cd42dbd8d06f88485d359c31c8e6b0ec503d
Saleh Trissi <rayatrissi1@gmail.com>
2025-03-21 11:21:30 +0100

    Add initial project structure with MVC architecture

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+// Load environment variables
+require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+// Access variables
+$db_host = $_ENV['DB_HOST'];
